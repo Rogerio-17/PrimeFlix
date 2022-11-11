4dd5fffd1fa5ac196751d2fe6db1f88047db4377
Rogerio-17 <rogeriojmf10@gmail.com>
2022-11-11 14:47:00 -0300

    adicionando border radios nas imagens

--- src/pages/semelhantes/index.js
+++ src/pages/semelhantes/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
-import "./index.css";
+import "./semelhantes.css";
 import { Link } from "react-router-dom";
 
 function Semelhantes() {
